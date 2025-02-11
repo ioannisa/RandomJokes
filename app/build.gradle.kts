10462cb839250704084f100d63248e47d47ad949
Ioannis Anifantakis <ianyfantakis@deloitte.gr>
2025-02-11 19:52:36 +0200

    First Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.com.google.devtools.ksp)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
 }
 
 android {
@@ -49,6 +51,45 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Additional Icons
+    implementation(libs.androidx.material.icons.extended)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.kotlinx.serialization.json)
+
+    // Koin
+    implementation(libs.bundles.koin)
+    implementation(libs.bundles.koin.compose)
+
+    // Coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil.gif)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    // Ktor
+    implementation(libs.bundles.ktor)
+
+    // Timber for logging
+    implementation(libs.timber)
+
+    // Google Fonts
+    // https://developer.android.com/develop/ui/compose/text/fonts#use-downloadable-fonts
+    implementation(libs.androidx.ui.text.google.fonts)
+
+    // Secured Android Persistence
+    implementation(libs.secure.persist)
+    implementation(libs.secure.persist.compose)
+
+    // SplashScreen
+    implementation(libs.androidx.core.splashscreen)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
