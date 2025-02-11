b7b595f9ab03da10132654c5ac06e922145f04da
Ioannis Anifantakis <ianyfantakis@deloitte.gr>
2025-02-11 20:38:17 +0200

    More idiomatic viewmodel retrieving cached jokes

--- app/src/main/java/eu/anifantakis/randomjokes/screens/JokeViewModel.kt
+++ app/src/main/java/eu/anifantakis/randomjokes/screens/JokeViewModel.kt
@@ -4,20 +4,25 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import eu.anifantakis.randomjokes.Joke
 import eu.anifantakis.randomjokes.repository.JokeRepository
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class JokeViewModel(
     private val repository: JokeRepository
 ) : ViewModel() {
     // Expose cached jokes as a StateFlow
-    val jokes: StateFlow<List<Joke>> = repository.getCachedJokes().stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.Lazily,
-        initialValue = emptyList()
-    )
+
+    private val _jokes = MutableStateFlow<List<Joke>>(emptyList())
+    val jokes = _jokes.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            repository.getCachedJokes().collect { jokesList ->
+                _jokes.value = jokesList
+            }
+        }
+    }
 
     fun refreshJokes() {
         viewModelScope.launch {
