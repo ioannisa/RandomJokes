e598c3d597163d4358c7301dbd77ad3b4cf93af4
Ioannis Anifantakis <ianyfantakis@deloitte.gr>
2025-02-11 20:22:43 +0200

    Koin Definitions for Ktor Http Client

--- app/src/main/java/eu/anifantakis/randomjokes/di/AppModule.kt
+++ app/src/main/java/eu/anifantakis/randomjokes/di/AppModule.kt
@@ -4,12 +4,37 @@ import androidx.room.Room
 import eu.anifantakis.randomjokes.database.AppDatabase
 import eu.anifantakis.randomjokes.repository.JokeRepository
 import eu.anifantakis.randomjokes.repository.JokeRepositoryImpl
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.android.ext.koin.androidApplication
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val appModule = module {
+
+    //  Ktor HttpClient
+    single {
+        HttpClient(CIO) {
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.ALL
+            }
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    isLenient = true
+                })
+            }
+        }
+    }
 
     // Database DI
     single {
